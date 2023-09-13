55cdc6db2173cb82d09f6f99ed86d2ebb72da773
Zalman-Philip <z0533147971@gmail.com>
2023-09-13 15:35:45 +0300

    commit

--- univercity2.sql
+++ univercity2.sql
@@ -1,38 +1,43 @@
-CREATE TABLE University.Enrollments (
-	enrollment_id serial PRIMARY KEY,
-	student_id INT, 
-	FOREIGN KEY (student_id) REFERENCES University.Students(student_id),
-	course_id INT,
-	FOREIGN KEY (course_id)  REFERENCES University.Courses (course_id));
-INSERT INTO University.Enrollments(student_id,course_id)
-VALUES('1','1'),
-('2','1'),
-('3','2'),
-('4','3'),
-('1','3');
+-- Active: 1694603789152@@127.0.0.1@5432@university
+CREATE TABLE
+    University.Enrollments (
+        enrollment_id serial PRIMARY KEY,
+        student_id INT,
+        FOREIGN KEY (student_id) REFERENCES University.Students(student_id),
+        course_id INT,
+        FOREIGN KEY (course_id) REFERENCES University.Courses (course_id)
+    );
+
+INSERT INTO
+    University.Enrollments(student_id, course_id)
+VALUES
+('1', '1'), ('2', '1'), ('3', '2'), ('4', '3'), ('1', '3');
+
 SELECT * FROM University.Enrollments;
-ALTER TABLE university.Enrollments 
-ADD CONSTRAINT uniqueRow UNIQUE(student_id,course_id)
 
 
-CREATE TABLE University.TeacherCourse(
-teacherCourse_id serial PRIMARY KEY,
-	course_id INT,
-	FOREIGN KEY (course_id) REFERENCES University.Courses(course_id),
-	teacher_id INT,
-	FOREIGN KEY (teacher_id) REFERENCES University.Teachers (teacher_id)
-);
+CREATE TABLE
+    University.TeacherCourse(
+        teacherCourse_id SERIAL PRIMARY KEY,
+        course_id INT,
+        FOREIGN KEY (course_id) REFERENCES University.Courses(course_id),
+        teacher_id INT,
+        FOREIGN KEY (teacher_id) REFERENCES University.Teachers (teacher_id)
+    );
+
 SELECT * FROM University.TeacherCourse;
 
+CREATE TABLE
+    University.Grades(
+        grade_id SERIAL PRIMARY KEY,
+        grade INT CHECK (grade <= 100),
+        enrollment_id INT,
+        FOREIGN KEY (enrollment_id) REFERENCES University.Enrollment (enrollment_id)
+    );
 
+INSERT INTO
+    University.Grades(grade, student_id, course_id)
+VALUES
+('67', '1'), ('98', '2',), ('100', '3'), ('95', '4'), ('89', '5');
 
-CREATE TABLE University.Grades(
-	grade_id serial PRIMARY KEY,
-	grade INT
-	CHECK (grade <= 100),
-    enrollment_id INT,
-    FOREIGN KEY (enrollment_id) REFERENCES University.Enrollment ( enrollment_id)
-);
-INSERT INTO University.Grades(grade,student_id,course_id)
-VALUES('67','1'),('98','2',),('100','3'),('95','4'),('89','5');
 SELECT * FROM University.Grades;
--- univercity3.sql
+++ univercity3.sql
@@ -1,13 +1,23 @@
-CREATE TABLE University.StudyGroup(
-    StudyGroup_id SERIAL PRIMARY KEY,
-    StudyGroup_name TEXT,
-);
-INSERT INTO University.StudyGroup(StudyGroup_name)
-VALUES('Math'),('Physics');
-CREATE TABLE University.StudentInStudyGroup(
-    StudentInStudyGroup_id SERIAL PRIMARY KEY,
-    StudyGroup_id INT,
-    FOREIGN KEY (StudyGroup_id) REFERENCES University.StudyGroup (StudyGroup_id),
-    student_id INT,
-    FOREIGN KEY (student_id) REFERENCES University.Students (student_id) 
-);
+CREATE TABLE
+    University.StudyGroup(
+        StudyGroup_id SERIAL PRIMARY KEY,
+        StudyGroup_name TEXT,
+    );
+
+INSERT INTO
+    University.StudyGroup(StudyGroup_name)
+VALUES('Math'), ('Physics');
+
+CREATE TABLE
+    University.StudentInStudyGroup(
+        StudentInStudyGroup_id SERIAL PRIMARY KEY,
+        StudyGroup_id INT,
+        FOREIGN KEY (StudyGroup_id) REFERENCES University.StudyGroup (StudyGroup_id),
+        student_id INT,
+        FOREIGN KEY (student_id) REFERENCES University.Students (student_id)
+    );
+
+ALTER TABLE
+    University.Enrollments
+ADD
+    CONSTRAINT uniqueRow UNIQUE(student_id, course_id)
